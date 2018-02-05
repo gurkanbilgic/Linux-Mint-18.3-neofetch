ac30dbb5f4054cbaa398bdc8d142281d52960bed
gurkanbilgic <36174253+gurkanbilgic@users.noreply.github.com>
2018-02-06 01:44:38 +0300

    Update neofetch-install.sh

--- neofetch-install.sh
+++ neofetch-install.sh
@@ -9,18 +9,18 @@
 
 
 # repo dosyasını ekle
-echo "# Repo sisteme ekleniyor"
+echo "# Repo sisteme ekleniyor..."
 
 sudo add-apt-repository -y ppa:dawidd0811/neofetch
 
 # eklenen repo'nun güncellenmesi
-echo "# Yüklenen repo güncelleniyor"
+echo "# Yüklenen repo güncelleniyor..."
 
 sudo apt-get -y update
 
 # Yükleme Komutu
-echo "# Yükleniyor"
+echo "# Yükleniyor..."
 
 sudo apt install neofetch -y
 
-echo "################################################################"
+echo "# Kurulum Tamamlandı."
